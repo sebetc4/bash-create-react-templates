6a9ae4fa57d40a7505923bbfad67079c6873be8f
sebetc4 <sebastienetcheto@gmail.com>
2024-08-13 16:15:32 +0200

    refactor: interface to type

--- templates/template.tsx
+++ templates/template.tsx
@@ -1,15 +1,15 @@
 // Styles
-import styles from './{{directory_name}}.module.scss';
+import styles from './{{directory_name}}.module.scss'
 // Libs
-import { FC } from 'react';
+import { type FC } from 'react'
 // App
 
-interface {{directory_name}}Props {}
+type {{directory_name}}Props = {}
 
 export const {{directory_name}}: FC<{{directory_name}}Props> = () => {
   return (
     <div className={styles.{{directory_name_lower}}}>
       {{directory_name}}
     </div>
-  );
+  )
 }
